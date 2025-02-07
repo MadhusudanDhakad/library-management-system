371904e0e3b8885d527a61dd277b9a087aa83148
Madhusudan Narwadiya <93048008+MadhusudanDhakad@users.noreply.github.com>
2025-02-07 08:54:34 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
   const navigate = useNavigate();
